3990506e458365d8483a61d33949140feeb7134d
Rick Yiu <rickyiu@google.com>
2020-05-06 11:27:25 +0800

    sched/fair: use actual cpu capacity to calculate boosted util
    Currently when calculating boosted util for a cpu, it uses a fixed
    value of 1024 for calculation. So when top-app tasks moved to LC,
    which has much lower capacity than BC, the freq calculated will be
    high even the cpu util is low. This results in higher power
    consumption, especially on arch which has more little cores than
    big cores. By replacing the fixed value of 1024 with actual cpu
    capacity will reduce the freq calculated on LC.
    
    Bug: 152925197
    Test: boosted util reduced on little cores
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I80cdd08a2c7fa5e674c43bfc132584d85c14622b
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6163,7 +6163,7 @@ static int wake_affine(struct sched_domain *sd, struct task_struct *p,
 struct reciprocal_value schedtune_spc_rdiv;
 
 static long
-schedtune_margin(unsigned long signal, long boost)
+schedtune_margin(unsigned long signal, long boost, long capacity)
 {
 	long long margin = 0;
 
@@ -6172,12 +6172,14 @@ schedtune_margin(unsigned long signal, long boost)
 	 *
 	 * The Boost (B) value is used to compute a Margin (M) which is
 	 * proportional to the complement of the original Signal (S):
-	 *   M = B * (SCHED_CAPACITY_SCALE - S)
+	 *   M = B * (capacity - S)
 	 * The obtained M could be used by the caller to "boost" S.
 	 */
 	if (boost >= 0) {
-		margin  = SCHED_CAPACITY_SCALE - signal;
-		margin *= boost;
+		if (capacity > signal) {
+			margin  = capacity - signal;
+			margin *= boost;
+		}
 	} else
 		margin = -signal * boost;
 
@@ -6197,7 +6199,7 @@ schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p)
 	if (boost == 0)
 		margin = 0;
 	else
-		margin = schedtune_margin(util, boost);
+		margin = schedtune_margin(util, boost, capacity_orig_of(cpu));
 
 	trace_sched_boost_cpu(cpu, util, margin);
 
@@ -6214,7 +6216,7 @@ long schedtune_task_margin(struct task_struct *task)
 		return 0;
 
 	util = task_util_est(task);
-	margin = schedtune_margin(util, boost);
+	margin = schedtune_margin(util, boost, SCHED_CAPACITY_SCALE);
 
 	return margin;
 }
@@ -6767,7 +6769,6 @@ static inline int select_idle_sibling_cstate_aware(struct task_struct *p,
 					goto next;
 
 				/* figure out if the task can fit here at all */
-				new_usage = boosted_task_util(p);
 				capacity_orig = capacity_orig_of(i);
 
 				if (new_usage > capacity_orig)
